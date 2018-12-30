fb0f9167527d25fb22bc92fdb67839aef6e5995d
Alexander Ignatyev <ignatyev.alexander@gmail.com>
2018-12-30 22:14:14 +0000

    [quora] use numpy arrays to train spacy classificator

--- Quora_Insincere_Questions/train_space_model.py
+++ Quora_Insincere_Questions/train_space_model.py
@@ -5,6 +5,8 @@ import os
 import spacy
 from spacy.util import minibatch, compounding
 
+from prepare_data import logfunc
+
 class Classifier:
     def __init__(self):
         self.nlp = spacy.load('en_core_web_sm')
@@ -24,75 +26,110 @@ class Classifier:
 
     @staticmethod
     def convert_data(quora_df):
-        texts = quora_df.question_text.tolist()
-        annotations = [{'POSITIVE': int(y)} for y in quora_df.target.tolist()]
-        return (texts, annotations)
+        annotations = quora_df.target.apply(lambda v: {'cats': {'POSITIVE': int(v)}})
+        df = pd.DataFrame({'texts': quora_df.question_text, 'annotations': annotations})
+        return df
 
 
-    def train(self, train_texts, train_cats, dev_texts, dev_cats, n_iter=20):
+    def train(self, train_df, test_df, n_iter=300):
         nlp = self.nlp
         textcat = self.textcat
         other_pipes = [pipe for pipe in nlp.pipe_names if pipe != 'textcat']
-        train_data = list(zip(train_texts, [{'cats': cats} for cats in train_cats]))
         with nlp.disable_pipes(*other_pipes):
             optimizer = nlp.begin_training()
+            optimizer.L2 = 500.0
+            self._print_optimizer_params(optimizer)
             print("Training the model...")
-            print('{:^5}\t{:^5}\t{:^5}\t{:^5}'.format('LOSS', 'P', 'R', 'F'))
+            print('{:^5}\t{:^5}\t{:^5}\t{:^5}\t{}'.format('LOSS', 'P', 'R', 'F1', 'Confusion Matrix'))
+            batch_size = compounding(400., 1000., 1.001)
             for i in range(n_iter):
                 losses = {}
                 # batch up the examples using spaCy's minibatch
-                batches = minibatch(train_data, size=compounding(4., 32., 1.001))
-                for batch in batches:
-                    texts, annotations = zip(*batch)
-                    nlp.update(texts, annotations, sgd=optimizer, drop=0.2, losses=losses)
-                with textcat.model.use_params(optimizer.averages):
-                    # evaluate on the dev data split off in load_data()
-                    scores = Classifier._evaluate(nlp.tokenizer, textcat, dev_texts, dev_cats)
-                print('{0:.3f}\t{1:.3f}\t{2:.3f}\t{3:.3f}'  # print a simple table
-                    .format(losses['textcat'], scores['textcat_p'],
-                            scores['textcat_r'], scores['textcat_f']))
+                batch = Classifier._get_batch(train_df, next(batch_size))
+                nlp.update(batch.texts, batch.annotations, sgd=optimizer, drop=0.2, losses=losses)
+
+                if i % 10 == 0:
+                    with textcat.model.use_params(optimizer.averages):
+                        # evaluate on the dev data split off in load_data()
+                        scores = Classifier._evaluate(nlp.tokenizer, textcat, test_df)
+                    print('{0:.3f}\t{1:.3f}\t{2:.3f}\t{3:.3f}\t{4}'  # print a simple table
+                        .format(losses['textcat'], scores['textcat_p'],
+                                scores['textcat_r'], scores['textcat_f'],
+                                scores['confusion_matrix']))
         nlp.to_disk(self.output_dir)
+    
+
+    @staticmethod
+    def _print_optimizer_params(optimizer):
+        print('Optimizer params:')
+        for k, v in optimizer.__dict__.items():
+            print('\t{}:'.format(k), v)
 
 
     @staticmethod
-    def _evaluate(tokenizer, textcat, texts, cats):
-        docs = (tokenizer(text) for text in texts)
-        tp = 0.0   # True positives
+    def _get_batch(df, batch_size):
+        nrows = df.shape[0]
+        row_numbers = np.random.choice(nrows, int(batch_size), replace = False)
+        return df.iloc[row_numbers]
+
+    @staticmethod
+    def _evaluate(tokenizer, textcat, df):
+
+        threshold = 0.5
+        golds = get_scores(df.annotations.values, 'POSITIVE')
+        docs = (tokenizer(text) for text in df.texts)
+
+        cat_scores = np.empty(df.shape[0])
+        for i, doc in enumerate(textcat.pipe(docs)):
+            cat_scores[i] = get_score_values({'cats': doc.cats}, 'POSITIVE')
+        
+        preds = v_score_to_pred(cat_scores, threshold)
+
+        tp = 0   # True positives
         fp = 1e-8  # False positives
         fn = 1e-8  # False negatives
-        tn = 0.0   # True negatives
-        for i, doc in enumerate(textcat.pipe(docs)):
-            gold = cats[i]
-            for label, score in doc.cats.items():
-                if label not in gold:
-                    continue
-                if score >= 0.5 and gold[label] >= 0.5:
-                    tp += 1.
-                elif score >= 0.5 and gold[label] < 0.5:
-                    fp += 1.
-                elif score < 0.5 and gold[label] < 0.5:
-                    tn += 1
-                elif score < 0.5 and gold[label] >= 0.5:
-                    fn += 1
+        tn = 1.0   # True negatives
+        for i in range(len(golds)):
+            if preds[i] >= threshold and golds[i] >= threshold:
+                tp += 1
+            elif preds[i] >= threshold and golds[i] < threshold:
+                fp += 1
+            elif preds[i] < threshold and golds[i] < threshold:
+                tn += 1
+            elif preds[i] < threshold and golds[i] >= threshold:
+                fn += 1
+
         precision = tp / (tp + fp)
         recall = tp / (tp + fn)
         if precision + recall != 0:
             f_score = 2 * (precision * recall) / (precision + recall)
         else:
             f_score = 0
-        return {'textcat_p': precision, 'textcat_r': recall, 'textcat_f': f_score}
+        return {'textcat_p': precision, 'textcat_r': recall, 'textcat_f': f_score, 'confusion_matrix': [tp, int(fp), tn, int(fn)] }
 
+
+def get_scores(annotations, cat_name):
+    return v_get_score_values(annotations, cat_name)
+
+
+def get_score_values(annotation, cat_name):
+    return float(annotation['cats'][cat_name])
+
+v_get_score_values = np.vectorize(get_score_values, otypes=[float])
+
+
+def score_to_pred(score, threshold):
+    return 0 if score < threshold else 1
+
+v_score_to_pred = np.vectorize(score_to_pred, otypes=[int])
 
 if __name__ == '__main__':
-    data_path = os.path.join('data', '.input', 'nrows_10000', 'preprocessed.csv')
-    quora_df = pd.read_csv(data_path, nrows=10_000)
+    data_path = os.path.join('data', '.input', 'nrows_100000', 'preprocessed.csv')
+    quora_df = Classifier.convert_data(pd.read_csv(data_path))
     nrows = quora_df.shape[0]
-    train_nrows = nrows - nrows // 5
+    train_nrows = nrows - nrows // 50
     train_df = quora_df.iloc[:train_nrows]
     test_df = quora_df.iloc[train_nrows:]
 
-    train_texts, train_cats = Classifier.convert_data(train_df)
-    test_texts, test_cats = Classifier.convert_data(test_df)
-
     clf = Classifier()
-    clf.train(train_texts, train_cats, test_texts, test_cats)
+    clf.train(train_df, test_df.iloc[:200])
