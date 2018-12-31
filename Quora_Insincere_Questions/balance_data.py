3789959d4aacb6fdd0453ecedeedab212b56ca68
Alexander Ignatyev <ignatyev.alexander@gmail.com>
2018-12-31 00:32:33 +0000

    [quora] add balance data script

---
+++ Quora_Insincere_Questions/balance_data.py
@@ -0,0 +1,41 @@
+"mitigate data skew issue"
+
+import os
+import pandas as pd
+import numpy as np
+
+
+def balance_data(quora_df, random_state = None):
+    np.random.seed(random_state)
+
+    print('before balancing, total cases:', quora_df.shape[0])
+    positive_df = quora_df.loc[quora_df.target == 1]
+    negative_df = quora_df.loc[quora_df.target == 0]
+
+    print('positive cases:', positive_df.shape[0], ', negative cases:', negative_df.shape[0])
+
+    positive_nrows = positive_df.shape[0]
+    negative_nrows = negative_df.shape[0]
+
+    indices = np.random.choice(negative_nrows, positive_nrows, replace = False)
+    negative_df = negative_df.values[indices]
+    data = np.vstack([positive_df.values, negative_df])
+    np.random.shuffle(data)
+    df = pd.DataFrame(data=data, columns = quora_df.columns)
+
+    print('after balancing, total cases:', df.shape[0])
+    positive_df = df.loc[df.target == 1]
+    negative_df = df.loc[df.target == 0]
+    print('positive cases:', positive_df.shape[0], ', negative cases:', negative_df.shape[0])
+
+    return df
+
+
+if __name__ == '__main__':
+    input_path = os.path.join('data', '.input', 'train.csv')
+    output_path = os.path.join('data', '.input', 'balanced_train.csv')
+
+    quora_df = pd.read_csv(input_path)
+    balanced_df = balance_data(quora_df)
+    balanced_df.to_csv(output_path, index = False)
+
