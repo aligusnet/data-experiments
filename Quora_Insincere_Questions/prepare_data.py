da6c893d39781e9d1b9501a4533f600378cbc5d2
Alexander Ignatyev <ignatyev.alexander@gmail.com>
2019-01-02 22:16:02 +0000

    [quora] refactor data preparation script

--- Quora_Insincere_Questions/prepare_data.py
+++ Quora_Insincere_Questions/prepare_data.py
@@ -11,10 +11,10 @@ from gensim.models.word2vec import LineSentence
 def logfunc(func):
     "Decorator that logs the geven function's runnng time"
 
-    def logged(*args):
+    def logged(*args, **kwargs):
         print('===> running', func.__name__, '...')
         t0 = time.perf_counter()
-        result = func(*args)
+        result = func(*args, **kwargs)
         elapsed = time.perf_counter() - t0
         print('<=== finished {} in {:03.2f} s.'.format(func.__name__, elapsed))
         return result
@@ -22,14 +22,9 @@ def logfunc(func):
 
 
 class Data:
-    def __init__(self, nrows, force):
-        self.nrows = nrows
+    def __init__(self, output_dir, force):
         self.force = force
-        self.data_dir =  os.path.join('data', '.input')
-        if self.nrows:
-            self.output_dir = os.path.join(self.data_dir, 'nrows_' + str(self.nrows))
-        else:
-            self.output_dir = os.path.join(self.data_dir, 'full')
+        self.output_dir = output_dir
     
         if not os.path.isdir(self.output_dir):
             os.mkdir(self.output_dir)
@@ -42,14 +37,17 @@ class Data:
         self.nlp = spacy.load('en_core_web_sm', disable=['ner', 'textcat'])
 
 
-    def prepare_data(self, filename = 'train.csv'):
-        quora_df = pd.read_csv(self._get_data_path(filename), nrows = self.nrows)
+    def fit(self, quora_df):
         clean_data_filepath = self._clean(quora_df)
-        bigram_model_filepath, trigram_model_filepath, sentences_filepath = self._build_phrase_models(clean_data_filepath)
-        bigram_model = Phrases.load(bigram_model_filepath)
-        trigram_model = Phrases.load(trigram_model_filepath)
+        sentences_filepath = self._build_phrase_models(clean_data_filepath)
 
-        return self._save_df(quora_df, sentences_filepath)
+        return self._build_df(quora_df, sentences_filepath)
+
+    def transform(self, quora_df):
+        self.force = True
+        clean_data_filepath = self._clean(quora_df)
+        sentences_path = self._transform_with_phrase_models(clean_data_filepath)
+        return self._build_df(quora_df, sentences_path)
 
 
     @logfunc
@@ -87,20 +85,18 @@ class Data:
 
         bigram_sentences_filepath = self._get_output_path('bigram_sentences.txt')
         trigram_sentences_filepath = self._get_output_path('trigram_sentences.txt')
-        bigram_model_filepath = self._get_output_path('bigram.model')
-        trigram_model_filepath = self._get_output_path('trigram.model')
 
-        self._train_phrase_detection_model(clean_data_filepath, bigram_sentences_filepath, bigram_model_filepath)
-        self._train_phrase_detection_model(bigram_sentences_filepath, trigram_sentences_filepath, trigram_model_filepath)
+        self.bigram_model = self._train_phrase_detection_model(clean_data_filepath, bigram_sentences_filepath)
+        self.trigram_model = self._train_phrase_detection_model(bigram_sentences_filepath, trigram_sentences_filepath)
 
-        return (bigram_model_filepath, trigram_model_filepath, trigram_sentences_filepath)
+        return trigram_sentences_filepath
 
 
-    def _train_phrase_detection_model(self, input_filepath, output_filepath, model_filepath):
+    def _train_phrase_detection_model(self, input_filepath, output_filepath):
         sentences = LineSentence(input_filepath)
         model = Phraser(Phrases(sentences))
-        model.save(model_filepath)
         self._save_sentences(sentences, model, output_filepath)
+        return model
 
 
     def _save_sentences(self, sents, model, filepath):
@@ -110,12 +106,18 @@ class Data:
                 f.write(new_sent + '\n')
 
 
+    def _transform_with_phrase_models(self, clean_data_filepath):
+        bigram_sentences_filepath = self._get_output_path('bigram_sentences2.txt')
+        trigram_sentences_filepath = self._get_output_path('trigram_sentences2.txt')
+
+        self._save_sentences(LineSentence(clean_data_filepath), self.bigram_model, bigram_sentences_filepath)
+        self._save_sentences(LineSentence(bigram_sentences_filepath), self.trigram_model, trigram_sentences_filepath)
+
+        return trigram_sentences_filepath
+
+
     @logfunc
-    def _save_df(self, quora_df, sentences_filepath):
-        result_filepath = self._get_output_path('preprocessed.csv')
-        if not self.force and os.path.isfile(result_filepath):
-            return result_filepath
-        
+    def _build_df(self, quora_df, sentences_filepath):
         nrows = quora_df.shape[0]
         sentences = self._read_sentences(sentences_filepath)
         if len(sentences) != nrows:
@@ -124,9 +126,10 @@ class Data:
                   ' must rebuild sentences')
             raise Exception('Unexpected number of sentences read')
         
-        result_df = pd.DataFrame({'qid': quora_df.qid, 'question_text': sentences, 'target': quora_df.target})
-        result_df.to_csv(result_filepath, index = False)
-        return result_filepath
+        data = {'qid': quora_df.qid, 'question_text': sentences}
+        if 'target' in quora_df:
+            data['target'] = quora_df.target
+        return pd.DataFrame(data)
 
 
     def _read_sentences(self, sentences_filepath):
@@ -135,14 +138,20 @@ class Data:
         return list(map(lambda line: line.rstrip(), lines))
 
 
-    def _get_data_path(self, filename):
-        return os.path.join(self.data_dir, filename)
-
-
     def _get_output_path(self, filename):
         return os.path.join(self.output_dir, filename)
-    
+
 
 if __name__ == '__main__':
-    data = Data(None, False)
-    data.prepare_data('balanced_train.csv')
+    nrows = None
+    data_path = os.path.join('data', '.input', 'balanced_train_4.csv')
+    output_dir = os.path.join('data', '.input', 'full' if nrows is None else str(nrows))
+    result_filepath = os.path.join(output_dir,'preprocessed.csv')
+
+    quora_df = pd.read_csv(data_path, nrows = nrows)
+    data = Data(output_dir, False)
+
+    df = data.fit(quora_df)
+    df.to_csv(result_filepath, index = False)
+
+    print(data.transform(quora_df[:100]))
