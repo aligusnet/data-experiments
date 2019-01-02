fe8c56c58f04221783f414f50175837102642a5d
Alexander Ignatyev <ignatyev.alexander@gmail.com>
2019-01-02 19:45:13 +0000

    [quora] balance data with multiplicator = 4

--- Quora_Insincere_Questions/balance_data.py
+++ Quora_Insincere_Questions/balance_data.py
@@ -5,7 +5,7 @@ import pandas as pd
 import numpy as np
 
 
-def balance_data(quora_df, mutiplicator = 1, random_state = None):
+def balance_data(quora_df, multiplicator = 1, random_state = None):
     np.random.seed(random_state)
 
     print('before balancing, total cases:', quora_df.shape[0])
@@ -17,7 +17,7 @@ def balance_data(quora_df, mutiplicator = 1, random_state = None):
     positive_nrows = positive_df.shape[0]
     negative_nrows = negative_df.shape[0]
 
-    indices = np.random.choice(negative_nrows, positive_nrows * mutiplicator, replace = False)
+    indices = np.random.choice(negative_nrows, positive_nrows * multiplicator, replace = False)
     negative_df = negative_df.values[indices]
     data = np.vstack([positive_df.values, negative_df])
     np.random.shuffle(data)
@@ -33,9 +33,9 @@ def balance_data(quora_df, mutiplicator = 1, random_state = None):
 
 if __name__ == '__main__':
     input_path = os.path.join('data', '.input', 'train.csv')
-    output_path = os.path.join('data', '.input', 'balanced_train.csv')
+    output_path = os.path.join('data', '.input', 'balanced_train_4.csv')
 
     quora_df = pd.read_csv(input_path)
-    balanced_df = balance_data(quora_df)
+    balanced_df = balance_data(quora_df, multiplicator = 4)
     balanced_df.to_csv(output_path, index = False)
 
